a247730d95479c047690d76db6bf4721035c77a0
Anna Nechaeva <115800504+a-nechaeva@users.noreply.github.com>
2024-03-31 19:28:52 +0300

    in proc

--- lab1/main.py
+++ lab1/main.py
@@ -1,16 +1,5 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from termcolor import colored
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+print(colored('aaaaaaaaaaa', 'red'))
+print("\033[1;32;40m Bright Green  \n")
--- lab1/task5.py
+++ lab1/task5.py
@@ -1,6 +1,13 @@
 import numpy as np
 from math import factorial
 from scipy.stats import norm
+from decimal import *
+from termcolor import colored
+
+
+#function gets name for each item in result
+_add_names_for_result = lambda _inp: {'Bernoulli ' : _inp[0], 'Poisson ' : _inp[1], 'Local Moivre-Laplace ' : _inp[2], 'Integral Moivre-Laplace ' : _inp[3]}
+
 
 # binomial coefficients counter
 bin_coef = lambda n, k: factorial(n) / (factorial(k) * factorial(n - k))
@@ -14,8 +21,12 @@ def bernoulli(k, p, n):
 # Poisson function
 def poisson(k, p, n):
     _lambda = p * n
+    try:
+      return Decimal(_lambda ** k * np.exp(-_lambda)) / Decimal(factorial(k))
 
-    return _lambda ** k * np.exp(-_lambda) / factorial(k)
+    except OverflowError:
+      print(colored('Attention! For this function probability can not be calculated by Poisson theorem!', 'red'))
+      return "Can't count Poisson function!"
 
 
 # integral Moivre--Laplace function
@@ -30,75 +41,100 @@ def local_ML(k, p, n):
 
 # k in some area of n / 2
 def k_in_area_counter(n, p):
-    magic_sqrt = np.sqrt(n * p * (1 - p))
-    k1 = int(n / 2 - magic_sqrt)
-    k2 = int(n / 2 + magic_sqrt)
 
-    _bern = 0
-    _pois = 0
-    _locM = 0
+  magic_sqrt = np.sqrt(n * p * (1 - p))
+  k1 = int(n / 2 - magic_sqrt)
+  k2 = int(n / 2 + magic_sqrt)
 
-    for k in range(k1, k2 + 1):
-        _bern += bernoulli(k, p, n)
-        _pois += poisson(k, p, n)
-        _locM += local_ML(k, p, n)
+  _bern = 0
+  _pois = 0
+  _locM = 0
 
-    _intM = integral_ML(k1, k2, p, n)
+  for k in range(k1, k2 + 1):
+    t = poisson(k, p, n)
 
-    res = {'Bernoulli ': _bern, 'Poisson ': _pois, 'Local Moivre--Laplace ': _locM, 'Integral Moivre--Laplace ': _intM}
+    if isinstance(t, str):
+      _pois = t
+      break
 
-    return res
+    _pois += t
+
+  for k in range(k1, k2 + 1):
+    _bern += bernoulli(k, p, n)
+    _locM += local_ML(k, p, n)
+
+  _intM = integral_ML(k1, k2, p, n)
+
+  res = [_bern, _pois, _locM, _intM]
+
+  return _add_names_for_result(res)
 
 
 # Sn leq 5 fun
 def k_leq_5_counter(n, p):
-    _bern = 0
-    _pois = 0
-    _locM = 0
 
-    for k in range(6):
-        _bern += bernoulli(k, p, n)
-        _pois += poisson(k, p, n)
-        _locM += local_ML(k, p, n)
+  _bern = 0
+  _pois = 0
+  _locM = 0
 
-    _intM = integral_ML(0, 5, p, n)
+  for k in range(6):
+    t = poisson(k, p, n)
 
-    res = {'Bernoulli ': _bern, 'Poisson ': _pois, 'Local Moivre--Laplace ': _locM, 'Integral Moivre--Laplace ': _intM}
+    if isinstance(t, str):
+      _pois = t
+      break
 
-    return res
+    _pois += t
+
+  for k in range(6):
+    _bern += bernoulli(k, p, n)
+    _locM += local_ML(k, p, n)
+
+  _intM = integral_ML(0, 5, p, n)
+
+  res = [_bern, _pois, _locM, _intM]
+
+  return _add_names_for_result(res)
 
 
 # for k max
 def k_max_prob(n, p):
-    max_k = int(n * p)
-    _bern = 0
-    _pois = 0
-    _locM = 0
+  max_k = int(n * p)
+  _bern = 0
+  _pois = 0
+  _locM = 0
 
-    for k in range(max_k):
-        _bern += bernoulli(k, p, n)
-        _pois += poisson(k, p, n)
-        _locM += local_ML(k, p, n)
 
-    _intM = integral_ML(0, max_k, p, n)
+  for k in range(max_k):
+    t = poisson(k, p, n)
 
-    res = {'Bernoulli ': _bern, 'Poisson ': _pois, 'Local Moivre--Laplace ': _locM, 'Integral Moivre--Laplace ': _intM}
+    if isinstance(t, str):
+      _pois = t
+      break
 
-    return res
+    _pois += t
+
+  for k in range(max_k):
+    _bern += bernoulli(k, p, n)
+    _locM += local_ML(k, p, n)
+
+  _intM = integral_ML(0, max_k, p, n)
+
+  res = [_bern, _pois, _locM, _intM]
+
+  return _add_names_for_result(res)
 
 
 def tester():
-    print("Enter n:")
-    n = int(input())
-    print("Enter p:")
-    p = float(input())
+    n = 1000
+    p = 0.01
 
     leq_5 = k_leq_5_counter(n, p)
 
     print('For P(Sn leq 5) \n')
 
     for key, value in leq_5.items():
-        print("{0}: {1}".format(key, value))
+      print("{0}: {1}".format(key,value))
 
     print('\n ------------------- \n')
 
@@ -107,7 +143,7 @@ def tester():
     in_area = k_in_area_counter(n, p)
 
     for key, value in in_area.items():
-        print("{0}: {1}".format(key, value))
+      print("{0}: {1}".format(key,value))
 
     print('\n ------------------- \n')
 
@@ -116,7 +152,7 @@ def tester():
     k_max = k_max_prob(n, p)
 
     for key, value in k_max.items():
-        print("{0}: {1}".format(key, value))
+      print("{0}: {1}".format(key,value))
 
 
 tester()
